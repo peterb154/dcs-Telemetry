7bd475c5b075e1e69d2923911db00b48e5b10088
Brian Peterson <brian@epetersons.com>
2021-11-24 23:43:25 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Dcs p51 Telemetetry
+# Dcs p51 Telemetry
 
 ## installation
 1. Put the [p51Telemetry.lua](./dcsTelemetry/p51Telemetry.lua) file somewhere
@@ -8,10 +8,8 @@
     dofile([[C:\Users\brian\Projects\dcs-Telemetry\dcsTelemetry\p51Telemetry.lua]])
     ```
 3. Go fly
-4. Should probably remove the export from the Export.lua when not in use.
 
 ## Usage
 1. Tool logs data every second
-2. Csv telemetry data will be saved in Saved Games \ DCS.xxx \ Logs \ DCS-p51Telemetry.log
-3. Open log file Excel, use a comma separator and graph away... All missions are saved into one file.. So might need
-   to trim the data
+2. Csv telemetry data will be saved in Saved Games \ DCS.xxx \ Logs \ DCS-p51Telemetry-DATE.csv after every mission
+3. Open with excel and chart away!
--- dcsTelemetry/p51Telemetry.lua
+++ dcsTelemetry/p51Telemetry.lua
@@ -14,7 +14,7 @@ function LuaExportStart()
     log.set_output(modName, modName, log.TRACE, log.MESSAGE)
     headers_printed = false
     local version = LoGetVersionInfo()
-    local pilot = LoGetPilotName() -- (args - 0, results - 1 (text string))
+    local pilot = LoGetPilotName()
     version = string.format("DCS Version: %d.%d.%d.%d",
             version.ProductVersion[1],
             version.ProductVersion[2],
@@ -45,10 +45,10 @@ function LuaExportStop()
     csvFile = lfs.writedir() .. "\Logs\\" .. modName .. "." .. timeStamp .. ".csv"
     log.write(modName, log.INFO, "Moving file: " .. logFile .. " to: " .. csvFile)
     os.execute('move "' .. logFile .. '" "' .. csvFile .. '"')
-    os.rename(logFile, csvFile)
 end
 
 function LuaExportActivityNextEvent(t)
+    -- runs every 1 second
     if not string.find(THIS_AIRCRAFT, TARGET_AIRCRAFT) then
         -- if this is not the target aircraft, get out
         return
@@ -73,65 +73,66 @@ function logEvent(message, level)
 end
 
 function getTelemetry()
+    -- commented out the data that just clutters the analysis for now
     mps2mph = 2.23694
     meter2feet = 3.28084
     rad2deg = 57.2958
     mm2inch = 0.0393701
     data = {}
-    data.IAS = round(LoGetIndicatedAirSpeed() * mps2mph, 0) -- (args - 0, results - 1 (m/s))
-    data.TAS = round(LoGetTrueAirSpeed() * mps2mph, 0) -- (args - 0, results - 1 (m/s))
-    data.TAS_10 = round(LoGetTrueAirSpeed() * mps2mph / 10, 1) -- (args - 0, results - 1 (m/s))
-    data.ASL = round(LoGetAltitudeAboveSeaLevel() * meter2feet, 0) -- (args - 0, results - 1 (meters))
-    data.AGL = round(LoGetAltitudeAboveGroundLevel() * meter2feet, 0) -- (args - 0, results - 1 (meters))
+    --data.IAS = round(LoGetIndicatedAirSpeed() * mps2mph, 0) -- (args - 0, results - 1 (m/s))
+    --data.TAS = round(LoGetTrueAirSpeed() * mps2mph, 0) -- (args - 0, results - 1 (m/s))
+    data.TAS_10 = round(LoGetTrueAirSpeed() * mps2mph / 10, 1)
+    --data.ASL = round(LoGetAltitudeAboveSeaLevel() * meter2feet, 0) -- (args - 0, results - 1 (meters))
+    data.ASL_100 = round(LoGetAltitudeAboveSeaLevel() * meter2feet / 100, 0)
+    --data.AGL = round(LoGetAltitudeAboveGroundLevel() * meter2feet, 0) -- (args - 0, results - 1 (meters))
     data.AOA = round(LoGetAngleOfAttack(), 1) -- (args - 0, results - 1 (rad))
 
-    self_data = LoGetSelfData()
-    data.Heading = round(self_data.Heading * rad2deg, 0)
-    data.Pitch = round(self_data.Pitch * rad2deg, 0)
-    data.Bank = round(self_data.Bank * rad2deg, 0)
+    --self_data = LoGetSelfData()
+    --data.Heading = round(self_data.Heading * rad2deg, 0)
+    --data.Pitch = round(self_data.Pitch * rad2deg, 0)
+    --data.Bank = round(self_data.Bank * rad2deg, 0)
 
-    accel = LoGetAccelerationUnits()
-    data.ACC_X = round(accel.x, 1)
-    data.ACC_Y = round(accel.y, 1)
-    data.ACC_Z = round(accel.z, 1)
+    --accel = LoGetAccelerationUnits()
+    --data.ACC_X = round(accel.x, 1)
+    --data.ACC_Y = round(accel.y, 1)
+    --data.ACC_Z = round(accel.z, 1)
 
-    data.VSI = round(LoGetVerticalVelocity() * meter2feet, 0) -- (args - 0, results - 1(m/s))
-    data.MACH = round(LoGetMachNumber(), 3)        -- (args - 0, results - 1)
-    data.BALL = LoGetSlipBallPosition()  -- (args - 0,results - 1)( -1 < result < 1)
+    --data.VSI = round(LoGetVerticalVelocity() * meter2feet, 0) -- (args - 0, results - 1(m/s))
+    data.VSI_100 = round(LoGetVerticalVelocity() * meter2feet / 100, 3)
+    --data.MACH = round(LoGetMachNumber(), 3)        -- (args - 0, results - 1)
+    --data.BALL = LoGetSlipBallPosition()  -- (args - 0,results - 1)( -1 < result < 1)
 
     mainPanel = GetDevice(0)
-    data.ALT_PRESS = round(map(mainPanel:get_argument_value(97), 0, 1, 28.1, 31.0), 2)
-    data.AHorizon_Pitch_DEG = round(map(mainPanel:get_argument_value(15), 1, -1, -math.pi / 3.0, math.pi / 3.0)*100, 1)
-    data.AHorizon_Bank_DEG = round(map(mainPanel:get_argument_value(14), 1, -1, -math.pi / 3.0, math.pi / 3.0)*180, 1)
+    --data.ALT_PRESS = round(map(mainPanel:get_argument_value(97), 0, 1, 28.1, 31.0), 2)
+    --data.AHorizon_Pitch_DEG = round(map(mainPanel:get_argument_value(15), 1, -1, -math.pi / 3.0, math.pi / 3.0)*100, 1)
+    --data.AHorizon_Bank_DEG = round(map(mainPanel:get_argument_value(14), 1, -1, -math.pi / 3.0, math.pi / 3.0)*180, 1)
     --data.GyroHeading = mainPanel:get_argument_value(12)
     --data.TurnNeedle = mainPanel:get_argument_value(27)
-    data.SlipBall = round(mainPanel:get_argument_value(28), 2)
-    data.FuelTankLeft = round(map(mainPanel:get_argument_value(155),0, 1, 0, 92), 1)
-    data.FuelTankRight = round(map(mainPanel:get_argument_value(155),0, 1, 0, 92), 1)
-    data.FuelTankFus = round(map(mainPanel:get_argument_value(160), 0, 1, 0, 85), 1)
-    data.FuelTankFus = round(map(mainPanel:get_argument_value(32), 0, 1, 0, 25), 1)
-    ----data.CompassHeading = mainPanel:get_argument_value(1)
-    ----data.CommandedCourse = mainPanel:get_argument_value(2)
-    ----data.CommandedCourseKnob = mainPanel:get_argument_value(3)
+    --data.SlipBall = round(mainPanel:get_argument_value(28), 2)
+    --data.FuelTankLeft = round(map(mainPanel:get_argument_value(155),0, 1, 0, 92), 1)
+    --data.FuelTankRight = round(map(mainPanel:get_argument_value(155),0, 1, 0, 92), 1)
+    --data.FuelTankFus = round(map(mainPanel:get_argument_value(160), 0, 1, 0, 85), 1)
+    --data.CompassHeading = mainPanel:get_argument_value(1)
+    --data.CommandedCourse = mainPanel:get_argument_value(2)
+    --data.CommandedCourseKnob = mainPanel:get_argument_value(3)
 
-    data.Hydraulic_Pressure = round(map(mainPanel:get_argument_value(78), 0, 1, 0, 2000), 1)
+    --data.Hydraulic_Pressure = round(map(mainPanel:get_argument_value(78), 0, 1, 0, 2000), 1)
     data.Manifold_Pressure = round(map(mainPanel:get_argument_value(10), 0, 1, 10, 75),1)
-    data.Engine_RPM = round(map(mainPanel:get_argument_value(23), 0, 1, 0, 4500),0)
+    --data.Engine_RPM = round(map(mainPanel:get_argument_value(23), 0, 1, 0, 4500),0)
     data.Engine_RPM_100 = round(map(mainPanel:get_argument_value(23), 0, 1, 0, 4500)/100,2)
-    data.Vacuum_Suction = round(map(mainPanel:get_argument_value(9), 0, 1, 0, 10), 1)
+    --data.Vacuum_Suction = round(map(mainPanel:get_argument_value(9), 0, 1, 0, 10), 1)
     data.Carb_Temperature = round(map(mainPanel:get_argument_value(21), 0, 1, -80, 150), 1)
     data.Coolant_Temperature = round(map(mainPanel:get_argument_value(22), 0, 1, -80, 150),1)
     data.Oil_Temperature = round(map(mainPanel:get_argument_value(30), 0, 1, 0, 100),1)
     data.Oil_Pressure = round(map(mainPanel:get_argument_value(31), 0, 1, 0, 200), 1)
     data.Accelerometer = round(map(mainPanel:get_argument_value(175), 0, 1, -5, 12), 1)
-    data.Ammeter = round(map(mainPanel:get_argument_value(101), 0, 1, 0, 150), 1)
-    data.WEP = mainPanel:get_argument_value(190)
+    --data.Ammeter = round(map(mainPanel:get_argument_value(101), 0, 1, 0, 150), 1)
+    --data.WEP = mainPanel:get_argument_value(190)
 
-    data.StickPitch_PCT = round(mainPanel:get_argument_value(50) * 100, 1)
-    data.StickBank_PCT = round(mainPanel:get_argument_value(51) * 100, 1)
-    data.RudderPedals = round(mainPanel:get_argument_value(54) * 100, 1)
-    data.Throttle = round(mainPanel:get_argument_value(43)* 100, 1)
-
+    --data.StickPitch_PCT = round(mainPanel:get_argument_value(50) * 100, 1)
+    --data.StickBank_PCT = round(mainPanel:get_argument_value(51) * 100, 1)
+    --data.RudderPedals = round(mainPanel:get_argument_value(54) * 100, 1)
+    --data.Throttle = round(mainPanel:get_argument_value(43)* 100, 1)
     return data
 end
 
